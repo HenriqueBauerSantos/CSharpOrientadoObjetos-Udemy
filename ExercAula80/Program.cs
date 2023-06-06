c93c1d0e3f477ae8d5e8b93a2d4ad2703054fb84
Henrique <henrique.x1@hotmail.com>
2023-06-06 20:06:14 -0300

    Adicionando atividades 80/81

---
+++ ExercAula80/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ExercAula80 {
+    class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Quantidade de numeros de Linhas e Colunas da matriz: ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    Console.WriteLine("Digite o elemento na posicao (" + i + "," + j + "):");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("   ");
+            Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < n; i++) {
+                Console.Write(mat[i, i]);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine("   ");
+            int negNum = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (mat[i, j] < 0) {
+                        negNum++;
+                    }
+                }
+            }
+            Console.Write("Negative numbers = "+negNum);
+        }
+    }
+}
---
+++ ExercAula81/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ExercAula81 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Write the number of matriz lines: ");
+            int linhas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Write the number of matriz columns: ");
+            int colunas = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[linhas, colunas];
+
+            for (int i = 0; i < mat.GetLength(0); i++) {
+                for (int j = 0; j < mat.GetLength(1); j++) {
+                    Console.WriteLine("Write the number of positon (" + i + "," + j + "):");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("  ");
+            Console.WriteLine("Write the number that be searsh: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("  ");
+            for (int i = 0; i < mat.GetLength(0); i++) {
+                for (int j = 0; j < mat.GetLength(1); j++) {
+                    if (mat[i, j] == num) {
+                        Console.WriteLine("Position: " + i + "," + j);
+                        if (i != 0) {
+                            Console.WriteLine("Up: " + mat[(i-1),j]);
+                        }
+                        if (i < mat.GetLength(0)) {
+                            Console.WriteLine("Down: " + mat[(i + 1), j]);
+                        }
+                        if (j != 0) {
+                            Console.WriteLine("Left: " + mat[i, (j - 1)]);
+                        }
+                        if ((j+1) < mat.GetLength(1)) {
+                            Console.WriteLine("Right: " + mat[i, (j + 1)]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
